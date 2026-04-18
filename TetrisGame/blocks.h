3a3536bf221ada3dd895a92cc6113ec3bbce7dec
bamo999 <safwanazam402@gmail.com>
2026-04-18 20:01:40 +0800

    Blocks done

--- TetrisGame/blocks.h
+++ TetrisGame/blocks.h
@@ -5,7 +5,7 @@
 
 using Pos = Position;
 
-class LBlock : public Block
+class LBlock: public Block
 {
 public:
 	LBlock() {
@@ -38,5 +38,185 @@ public:
 			Pos{1, 1},
 			Pos{2, 1},
 		};
-	}
+	};
+};
+
+class JBlock : public Block {
+public:
+	JBlock() {
+		cells[0] = {
+			Pos{0, 0},
+			Pos{1, 0},
+			Pos{1, 1},
+			Pos{1, 2},
+		};
+		cells[1] = {
+			Pos{0, 1},
+			Pos{0, 2},
+			Pos{1, 1},
+			Pos{2, 1},
+		};
+		cells[1] = {
+			Pos{1, 0},
+			Pos{1, 1},
+			Pos{1, 2},
+			Pos{2, 2},
+		};
+		cells[1] = {
+			Pos{0, 1},
+			Pos{1, 1},
+			Pos{2, 0},
+			Pos{2, 1},
+		};
+	};
+};
+
+class IBlock : public Block {
+public:
+	IBlock() {
+		cells[0] = {
+			Pos{1, 0},
+			Pos{1, 1},
+			Pos{1, 2},
+			Pos{1, 3},
+		};
+		cells[1] = {
+			Pos{0, 2},
+			Pos{1, 2},
+			Pos{2, 2},
+			Pos{3, 2},
+		};
+		cells[1] = {
+			Pos{2, 0},
+			Pos{2, 1},
+			Pos{2, 2},
+			Pos{2, 3},
+		};
+		cells[1] = {
+			Pos{0, 1},
+			Pos{1, 1},
+			Pos{2, 1},
+			Pos{3, 1},
+		};
+	};
+};
+
+class OBlock : public Block {
+public:
+	OBlock() {
+		cells[0] = {
+			Pos{0, 0},
+			Pos{0, 1},
+			Pos{1, 0},
+			Pos{1, 1},
+		};
+		cells[1] = {
+			Pos{0, 0},
+			Pos{0, 1},
+			Pos{1, 0},
+			Pos{1, 1},
+		};
+		cells[1] = {
+			Pos{0, 0},
+			Pos{0, 1},
+			Pos{1, 0},
+			Pos{1, 1},
+		};
+		cells[1] = {
+			Pos{0, 0},
+			Pos{0, 1},
+			Pos{1, 0},
+			Pos{1, 1},
+		};
+	};
+};
+
+class SBlock : public Block {
+public:
+	SBlock() {
+		cells[0] = {
+			Pos{0, 1},
+			Pos{0, 2},
+			Pos{1, 0},
+			Pos{1, 1},
+		};
+		cells[1] = {
+			Pos{0, 1},
+			Pos{1, 1},
+			Pos{1, 2},
+			Pos{2, 2},
+		};
+		cells[1] = {
+			Pos{1, 1},
+			Pos{1, 2},
+			Pos{2, 0},
+			Pos{2, 1},
+		};
+		cells[1] = {
+			Pos{0, 0},
+			Pos{1, 0},
+			Pos{1, 1},
+			Pos{2, 1},
+		};
+	};
+};
+
+class TBlock : public Block {
+public:
+	TBlock() {
+		cells[0] = {
+			Pos{0, 1},
+			Pos{1, 0},
+			Pos{1, 1},
+			Pos{1, 2},
+		};
+		cells[1] = {
+			Pos{0, 1},
+			Pos{1, 1},
+			Pos{1, 2},
+			Pos{2, 1},
+		};
+		cells[1] = {
+			Pos{1, 0},
+			Pos{1, 1},
+			Pos{1, 2},
+			Pos{2, 1},
+		};
+		cells[1] = {
+			Pos{0, 1},
+			Pos{1, 0},
+			Pos{1, 1},
+			Pos{2, 1},
+		};
+	};
+};
+
+class ZBlock : public Block {
+public:
+	ZBlock() {
+		cells[0] = {
+			Pos{0, 1},
+			Pos{0, 1},
+			Pos{1, 1},
+			Pos{1, 2},
+		};
+		cells[1] = {
+			Pos{0, 2},
+			Pos{1, 1},
+			Pos{1, 2},
+			Pos{2, 1},
+		};
+		cells[1] = {
+			Pos{1, 0},
+			Pos{1, 1},
+			Pos{2, 1},
+			Pos{2, 2},
+		};
+		cells[1] = {
+			Pos{0, 1},
+			Pos{1, 0},
+			Pos{1, 1},
+			Pos{2, 0},
+		};
+	};
 };
